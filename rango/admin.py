421e4d4a5a6abb69ef3ffa815032acd886cdff25
“Ruxandra-Ioana <r.i.mocanu@gmail.com>
2026-02-09 19:36:44 +0100

    chap 7

--- rango/admin.py
+++ rango/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from rango.models import Category, Page
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','views','likes')
+    prepopulated_fields = {'slug':('name',)}
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category', 'url')
 
