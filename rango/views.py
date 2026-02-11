8f62db6c3cdc787d97c4425719c1291608680712
“Ruxandra-Ioana <r.i.mocanu@gmail.com>
2026-02-11 16:04:44 +0100

    chap_8_ex_1_fin

--- rango/views.py
+++ rango/views.py
@@ -38,7 +38,7 @@ def add_category(request):
         form = CategoryForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
@@ -50,7 +50,7 @@ def add_page(request, category_name_slug):
         category = None
     # You cannot add a page to a Category that does not exist...
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
     form = PageForm()
     if request.method == 'POST':
         form = PageForm(request.POST)
